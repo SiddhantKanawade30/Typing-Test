00daa8a2ca1bb01f212f0746f0b037f75a8bda35
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-02-08 14:26:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ wpmTag = document.querySelector(".wpm span");
 
 
 let timer,
-maxTime = 60,
+maxTime = prompt("Enter the desired time in seconds  and click on start"),
 timeLeft = maxTime,
 charIndex = mistakes = isTyping = 0;
 
@@ -51,23 +51,23 @@ function initTyping() {
         characters.forEach(span => span.classList.remove("active"));
         characters[charIndex].classList.add("active");
 
-        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        const wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * maxTime);
         wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
         
         wpmTag.innerText = wpm;
         mistakeTag.innerText = mistakes;
-        cpmTag.innerText = charIndex - mistakes;
+       
     } else {
         clearInterval(timer);
         inpField.value = "";
-    }   
+    }
 }
 
 function initTimer() {
     if(timeLeft > 0) {
         timeLeft--;
         timeTag.innerText = timeLeft;
-        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * maxTime);
         wpmTag.innerText = wpm;
     } else {
         clearInterval(timer);
@@ -83,9 +83,80 @@ function resetGame() {
     timeTag.innerText = timeLeft;
     wpmTag.innerText = 0;
     mistakeTag.innerText = 0;
-    cpmTag.innerText = 0;
+   
 }
 
 loadParagraph();
 inpField.addEventListener("input", initTyping);
 tryButton.addEventListener("click", resetGame);
+
+// popup---------------------------------------------------------------------------------------------------
+
+const share = document.getElementById('share-btn')
+const container = document.getElementById("container")
+const close = document.getElementById("close")
+const input = document.getElementById('input')
+const btn = document.getElementById('btn')
+const TimeLeft = document.getElementById('TimeLeft')
+
+
+function showPopupCard() {
+    const overlay = document.getElementById("container");
+    overlay.style.display = "flex";
+}
+
+
+function closePopupCard() {
+    const overlay = document.getElementById("container");
+    overlay.style.display = "none"; 
+}
+
+
+window.onload = function() {
+    setTimeout(showPopupCard,(maxTime*1000+6000));  
+};
+
+
+ const wp = document.getElementById('whatsapp')
+ const tw = document.getElementById('twitter')
+ const sp = document.getElementById('snapchat')
+ const insta = document.getElementById('instagram')
+ const pinterest = document.getElementById('pinterest')
+ const fb = document.getElementById('fb')
+ const tele = document.getElementById('telegram')
+
+
+
+
+const pageUrl = location.href;
+const message = `Hey I just completed my typing test check it out at ${pageUrl}`;    
+
+
+const whatsappApi = `https://wa.me/?text= ${message}`
+const twitterApi =  `https://twitter.com/intent/tweet?text= ${message}`
+const instaApi = `https://www.instagram.com/direct/new/`
+const fbApi = `https://www.facebook.com/share.php?u= ${message}`
+const pintApi = `http://pinterest.com/pin/create/button/?url= ${message}`
+const telegramApi = `https://telegram.me/share/url?url=${pageUrl}&text=${message}`
+
+
+
+wp.addEventListener("click",()=>{
+    window.open(url=whatsappApi,target="_blank")
+})
+tw.addEventListener("click",()=>{
+    window.open(url=twitterApi,target="_blank")
+})
+insta.addEventListener("click",()=>{
+    window.open(url=instaApi,target="_blank")
+})
+pinterest.addEventListener("click",()=>{
+    window.open(url=pintApi,target="_blank")
+})
+fb.addEventListener("click",()=>{
+    window.open(url=fbApi,target="_blank")
+})
+tele.addEventListener("click",()=>{
+    window.open(url=telegramApi,target="_blank")
+})
+
